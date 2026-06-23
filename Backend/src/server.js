0ea27f7554d9b22b305f3d6b6aeb5caca243595c
Rajveer230 <rajveersingh230310@gmail.com>
2026-06-23 11:30:10 +0530

    initial commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
-const app=require("./app");
-const connectDB=require("./config/database");
+
+const app = require("./app");
+const connectDB = require("./config/database");
+
 connectDB();
-const PORT=3000;
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
